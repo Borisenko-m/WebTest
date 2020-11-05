3f55adfd3d1aa1d3c1f33803b39542a6be2399df
Borisenko <davildamage@gmail.com>
2020-11-05 10:27:08 +0300

    another xls test
    another xls test

--- WebTest/Classes/ExcelIO/ExcelIO.cs
+++ WebTest/Classes/ExcelIO/ExcelIO.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using FlexCel.Core;
+using FlexCel.XlsAdapter;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
-using OfficeOpenXml;
-using OfficeOpenXml.Drawing.Chart;
 using WebTest.Models;
 namespace WebTest.Classes.ExcelIO
 {
@@ -16,8 +16,11 @@ namespace WebTest.Classes.ExcelIO
         /// <param name="name">Name of file.</param>
         public ExcelIO(string name, string content)
         {
+            Xls = new XlsFile(1, TExcelFileFormat.v2016, true);
 
             var path = @$"..\{name}.xlsx";
+
+
             FileStream = File.Exists(path) ?
                 new FileStream(path, FileMode.Truncate, FileAccess.ReadWrite) :
                 new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
@@ -25,45 +28,42 @@ namespace WebTest.Classes.ExcelIO
             Path = FileStream.Name;
         }
 
-
+        XlsFile Xls;
         public string Path { get; set; }
         FileStream FileStream { get; set; }
         public void Test()
         {
-            using var package = new ExcelPackage(FileStream);
-            var sheet = package.Workbook.Worksheets.Add("My Sheet");
-            
             for (int i = 1; i <= 10; i++)
             {
                 for (int j = 1; j <= 10; j++)
                 {
-                    sheet.SetValue(i, j, i + " " + j);
+                    Xls.SetCellValue(i, j, i + " " + j);
                 }
             }
+            Xls.Save(FileStream, TFileFormats.Xlsx);
 
-            package.Save();
             FileStream.Close();
         }
         public void Execute(IEnumerable<Model> models)
         {
-            var type = typeof(Model);
-            IEnumerable<object> fields = default;
-            var i = 1;
-            var j = 1;
-            using var package = new ExcelPackage(FileStream);
-            var sheet = package.Workbook.Worksheets.Add("My Sheet");
-            foreach (var model in models)
-            {
-                fields = type.GetFields().Select(l => l.GetValue(model));
-                foreach (var item in fields)
-                {
-                    sheet.SetValue(i, j++, item);
-                }
-                i++;
-            }
-            // Save to file
-            package.Save();
-            FileStream.Close();
+            //var type = typeof(Model);
+            //IEnumerable<object> fields = default;
+            //var i = 1;
+            //var j = 1;
+            //using var package = new ExcelPackage(FileStream);
+            //var sheet = package.Workbook.Worksheets.Add("My Sheet");
+            //foreach (var model in models)
+            //{
+            //    fields = type.GetFields().Select(l => l.GetValue(model));
+            //    foreach (var item in fields)
+            //    {
+            //        sheet.SetValue(i, j++, item);
+            //    }
+            //    i++;
+            //}
+            //// Save to file
+            //package.Save();
+            //FileStream.Close();
         }
     }
 }
